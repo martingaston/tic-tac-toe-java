b28e23c14798a889538077e6c4264857d48f7b7a
Martin Gaston <squidman@gmail.com>
2019-04-09 10:37:31 +0100

    Add basic test for Player class

---
+++ src/main/java/Player.java
@@ -0,0 +1,11 @@
+public class Player {
+    private String symbol;
+
+    public Player(String playerSymbol) {
+        symbol = playerSymbol;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+}
