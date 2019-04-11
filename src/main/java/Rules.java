b434b951ff8082a9cd54000c6ceb32b6495b3ff8
Martin Gaston <squidman@gmail.com>
2019-04-11 23:32:25 +0100

    Refactor Rules and extract playerHasCompleteRow method

--- src/main/java/Rules.java
+++ src/main/java/Rules.java
@@ -1,43 +1,38 @@
 public class Rules {
     private Board board;
-    private int[][] winningMoves = {
-        {0,1,2},
-        {3,4,5},
-        {6,7,8},
-        {0,3,6},
-        {1,4,7},
-        {2,5,8},
-        {0,4,8},
-        {2,4,6}
-    };
-    private int totalWinningMoves = winningMoves.length;
-
-    public Rules() {
-
-    }
-
     public Rules(Board board) {
         this.board = board;
     }
 
     public boolean hasWinningMove(Player player) {
         boolean matchFound = false;
-        String[] boardState = this.board.getCurrentBoard();
         String playerSymbol = player.getSymbol();
 
-        for (int i = 0; i < totalWinningMoves;i++) {
-            int position1 = winningMoves[i][0];
-            int position2 = winningMoves[i][1];
-            int position3 = winningMoves[i][2];
+        int[][] winningMoves = {
+                {0,1,2},
+                {3,4,5},
+                {6,7,8},
+                {0,3,6},
+                {1,4,7},
+                {2,5,8},
+                {0,4,8},
+                {2,4,6}
+        };
 
-            if (boardState[position1].equals(playerSymbol) &&
-                boardState[position2].equals(playerSymbol) &&
-                boardState[position3].equals(playerSymbol)) {
+        for (int[] move : winningMoves) {
+            if (playerHasCompleteRow(move, playerSymbol)) {
                 matchFound = true;
             }
         }
 
         return matchFound;
+    }
+
+    private boolean playerHasCompleteRow(int[] move, String playerSymbol) {
+        String[] boardState = this.board.getCurrentBoard();
+        return boardState[move[0]].equals(playerSymbol) &&
+                boardState[move[1]].equals(playerSymbol) &&
+                boardState[move[2]].equals(playerSymbol);
     }
 
     public boolean gameIsOver() {
