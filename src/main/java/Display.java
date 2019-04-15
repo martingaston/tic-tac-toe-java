c07cd46ccec757ab1cf1583c849858a3d419ef8f
Martin Gaston <squidman@gmail.com>
2019-04-15 17:12:12 +0100

    extract methods from Display class

--- src/main/java/Display.java
+++ src/main/java/Display.java
@@ -2,6 +2,8 @@ import java.util.Arrays;
 
 class Display {
     private Board board;
+    private String newline = "\n";
+    private String divider = "+-----------+" + newline;
 
     public Display(Board board) {
         this.board = board;
@@ -12,21 +14,21 @@ class Display {
     }
 
     void showBoard() {
-        System.out.print(renderBoardFromState());
+        String[][] boardInRows = processBoardIntoRows();
+        System.out.print(renderRows(boardInRows));
     }
 
-    private String newline = "\n";
-    private String divider = "+-----------+" + newline;
-
-    private String renderBoardFromState() {
+    private String[][] processBoardIntoRows() {
         String[] boardState = this.board.getCurrentBoard();
 
-        String[][] rows = new String[][] {
+        return new String[][]{
                 Arrays.copyOfRange(boardState, 0, 3),
                 Arrays.copyOfRange(boardState, 3, 6),
                 Arrays.copyOfRange(boardState, 6, 9)
         };
+    }
 
+    private String renderRows(String[][] rows) {
         String grid = divider;
 
         for (int i = 0; i < rows.length; i++) {
