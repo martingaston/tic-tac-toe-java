4e6773215c28d498c1e2cd1b92abaf2c62b41c07
Martin Gaston <squidman@gmail.com>
2019-05-08 14:57:44 +0100

    add comments for regexps for parsing args in main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        //TODO ^--game=(.*)$ // regexp for GAME MODE
+        //TODO ^--board=(.*)$ // regexp for BOARD SIZE
+
         Game game = new Game();
         game.play();
         System.exit(0);
