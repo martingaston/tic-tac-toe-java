ca2d2efed6e1af458731a84d26ef4fee243f23af
Martin Gaston <35241716+martingaston@users.noreply.github.com>
2019-05-09 22:41:36 +0100

    Sprint5/integration test (#23)
    * refactor board to incorporate responsibilities from rules
    
    * add logic for dynamically created diagonals on board
    
    * add toString override to Line and test for each win condition
    
    * add Generic support to Line
    
    * remove generic support and stitch cells into Line hasWinner
    
    * add occupant to return Player from Cell and support for lines of any length in line hasWinner
    
    * add test for game being over
    
    * add boardIsFull method and gameIsOver will check for win and draw
    
    * add availableMoves to Board
    
    * remove Rules from Minimax
    
    * remove rules from other classes
    
    * format code
    
    * add Board.copy() method
    
    * remove unneeded paramters from Game
    
    * refactor Messages to be static and remove overly complex HashMaps for simpler String returns
    
    * format code
    
    * add IO class for E2E testing
    
    * add support for multiple board sizes in Board.copy() method
    
    * refactor out cell.isNotOccupied method
    
    * replace forEach statements in Board and Line with declarative streams
    
    * add integration tests for HVH win, lose and draw on 3x3 and 4x4 boards

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,33 +1,72 @@
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.*;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static org.junit.Assert.*;
 
 public class GameTest {
-    Game game;
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
 
     @Before
-    public void setUp() throws Exception {
-        game = new Game();
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void restoreStreams() {
+        System.setOut(originalOut);
     }
 
     @Test
-    public void parseArgsParsesValidModeArg() {
-        Map<String, String> expectedArgs = new HashMap<>(Map.of("mode", "hvc-hard"));
-        assertEquals(expectedArgs, game.parseArgs(new String[] {"--mode=hvc-hard"}));
+    public void testCrossWinsHVHGameOn3x3Board() {
+        String gameInputs = "1\n1\n1\n4\n2\n5\n3";
+        InputStream in = new ByteArrayInputStream(gameInputs.getBytes());
+        System.setIn(in);
+
+        Game.play(new String[] {});
+
+        String gameOutput = outContent.toString();
+
+        Pattern playerCrossWins = Pattern.compile("^.*(Player X wins!)$", Pattern.MULTILINE);
+        Matcher gameMatch = playerCrossWins.matcher(gameOutput);
+        assertTrue(gameMatch.find());
     }
 
     @Test
-    public void parseArgsParsesValidBoardArg() {
-        Map<String, String> expectedArgs = new HashMap<>(Map.of("board", "4x4"));
-        assertEquals(expectedArgs, game.parseArgs(new String[] {"--board=4x4"}));
+    public void testDrawStateEndsInDraw() {
+        String gameInputs = "1\n1\n1\n5\n2\n3\n7\n4\n6\n9\n8";
+        InputStream in = new ByteArrayInputStream(gameInputs.getBytes());
+        System.setIn(in);
+
+        Game.play(new String[] {});
+
+        String gameOutput = outContent.toString();
+
+        Pattern drawState = Pattern.compile("^.*(Bad luck! It's a draw!)$", Pattern.MULTILINE);
+        Matcher gameMatch = drawState.matcher(gameOutput);
+        assertTrue(gameMatch.find());
     }
 
     @Test
-    public void parseArgsParsesValidBoardAndModeArg() {
-        Map<String, String> expectedArgs = new HashMap<>(Map.of("board", "4x4", "mode", "cvc"));
-        assertEquals(expectedArgs, game.parseArgs(new String[] {"--mode=cvc", "--board=4x4"}));
+    public void testNoughtWinsHVHGameOn4x4BoardAfterCrossPlaysIncorrectMove() {
+        String gameInputs = "2\n1\n16\n2\n11\n3\n6\n1\n1\n5\n4";
+        InputStream in = new ByteArrayInputStream(gameInputs.getBytes());
+        System.setIn(in);
+
+        Game.play(new String[] {});
+
+        String gameOutput = outContent.toString();
+
+        Pattern playerCrossWins = Pattern.compile("^.*(Player O wins!)$", Pattern.MULTILINE);
+        Matcher gameMatch = playerCrossWins.matcher(gameOutput);
+        assertTrue(gameMatch.find());
     }
 }
