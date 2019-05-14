73ecce8cb64f25f6050266ef8b996e9553e44dcb
Martin Gaston <squidman@gmail.com>
2019-05-14 15:45:32 +0100

    remove unused Shutdown class & thread

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,6 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Runtime runtime = Runtime.getRuntime();
-        runtime.addShutdownHook(new Shutdown());
-
         Settings settings = new Settings(args);
         Game.play(settings);
     }
--- src/main/java/Shutdown.java
+++
@@ -1,5 +0,0 @@
-class Shutdown extends Thread {
-    public void run() {
-        System.out.println(Messages.shutdown());
-    }
-}
