2875fa799b96577afee43d4e0e6dce68c3212657
Martin Gaston <squidman@gmail.com>
2019-05-16 10:48:11 +0100

    remove unused method from PopulatedBoard

--- src/main/java/PopulatedBoard.java
+++ src/main/java/PopulatedBoard.java
@@ -1,5 +1,3 @@
-import java.util.List;
-
 public class PopulatedBoard {
     public static Board from(GameState state) {
         if(state.contents().size() != 9 && state.contents().size() != 16) {
@@ -15,26 +13,6 @@ public class PopulatedBoard {
                 newBoard.add(i, state.players().playerCross());
             } else if (currentSymbol.equals(state.players().playerNought().getSymbol())) {
                 newBoard.add(i, state.players().playerNought());
-            }
-        }
-
-        return newBoard;
-    }
-
-    public static Board from(List<String> boardList, Player playerCross, Player playerNought) {
-        if(boardList.size() != 9 && boardList.size() != 16) {
-            return new Board();
-        }
-
-        int sideLength = (int) Math.sqrt(boardList.size());
-        Board newBoard = new Board(sideLength);
-
-        for (int i = 0; i < boardList.size(); i++) {
-            String currentSymbol = boardList.get(i);
-            if (currentSymbol.equals(playerCross.getSymbol())) {
-                newBoard.add(i, playerCross);
-            } else if (currentSymbol.equals(playerNought.getSymbol())) {
-                newBoard.add(i, playerNought);
             }
         }
 
