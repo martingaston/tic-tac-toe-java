88d6438340c3bab6d64792c40377fd7b0b077283
Martin Gaston <squidman@gmail.com>
2019-05-16 11:36:28 +0100

    remove unused code from Settings and unused tests

--- src/main/java/Settings.java
+++ src/main/java/Settings.java
@@ -1,10 +1,7 @@
 import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Settings {
     private final List<String> gameSettings = new LinkedList<>();
-    private IO io;
     private Board board;
     private Display display;
     private Players players;
@@ -12,42 +9,30 @@ public class Settings {
     private Player playerNought;
 
     Settings(String[] args) {
-        io = new IO(new Scanner(System.in));
+        IO io = new IO(new Scanner(System.in));
         List<String> prevState = IO.gameIn();
 
+        GameState state;
         if (!prevState.isEmpty()) {
-            processSavedGame(prevState);
+            state = new FileState(prevState, io);
         } else if (args.length > 0) {
-            processArgsFromState(args);
+            state = new ArgState(args, io);
         } else {
-            processInputFromState(io);
-        }
-    }
-
-    public static Map<String, String> parseArgs(String[] args) {
-        Pattern argStructure = Pattern.compile("^--(\\w+)=([\\w|-]+)$");
-        Map<String, String> argMap = new HashMap<>();
-
-        for (String arg : args) {
-            Matcher matchedArg = argStructure.matcher(arg);
-            if (matchedArg.matches()) {
-                argMap.put(matchedArg.group(1), matchedArg.group(2));
-            }
+            state = new InputState(io);
         }
 
-        return argMap;
+        processState(state);
     }
 
-    private void processSavedGame(List<String> prevState) {
-        GameState fileState = new FileState(prevState);
-        board = fileState.board();
-        players = fileState.players();
-        playerCross = fileState.players().playerCross();
-        playerNought = fileState.players().playerNought();
+    private void processState(GameState state) {
+        board = state.board();
+        players = state.players();
+        playerCross = state.players().playerCross();
+        playerNought = state.players().playerNought();
         display = new Display(board);
 
         //TODO: these three lines should not be needed when FileState is fully integrated
-        createSettings(BoardModes.id(fileState.boardMode()), GameModes.id(fileState.mode()));
+        createSettings(BoardModes.id(state.boardMode()), GameModes.id(state.mode()));
     }
 
     private void createSettings(int boardNumber, int modeNumber) {
@@ -55,28 +40,6 @@ public class Settings {
         gameSettings.add(Integer.toString(modeNumber));
         gameSettings.add(playerCross.getSymbol());
         gameSettings.add(playerNought.getSymbol());
-    }
-
-    private void processArgsFromState(String[] args) {
-        GameState argState = new ArgState(args);
-        board = argState.board();
-        players = argState.players();
-        playerCross = argState.players().playerCross();
-        playerNought = argState.players().playerNought();
-        display = new Display(board);
-
-        createSettings(BoardModes.id(argState.boardMode()), GameModes.id(argState.mode()));
-    }
-
-    private void processInputFromState(IO io) {
-        GameState inputState = new InputState(io);
-        board = inputState.board();
-        players = inputState.players();
-        playerCross = inputState.players().playerCross();
-        playerNought = inputState.players().playerNought();
-        display = new Display(board);
-
-        createSettings(BoardModes.id(inputState.boardMode()), GameModes.id(inputState.mode()));
     }
 
     public Board board() {
